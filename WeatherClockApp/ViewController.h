4cd02fc565bad8a005c831a60efb0acb279a5222
刘林 <liulin@liulindeMac.local>
2015-10-23 08:44:31 -0700

    Create project

---
+++ WeatherClockApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeatherClockApp
+//
+//  Created by 刘林 on 15/10/23.
+//  Copyright (c) 2015年 刘林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
